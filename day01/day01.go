cc5bb0a2c0f2fe3e4199f0f9224b3d50613323a9
zuko <Joshua.B.Thomas@outlook.com>
2023-12-29 15:51:25 +0800

    Day01 - Part Two Complete

--- day01/day01.go
+++ day01/day01.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/szuduko/aoc-go/util"
 )
@@ -32,4 +33,78 @@ func Part1() {
 
 func Part2() {
 	fmt.Println("Part 2")
+	lines := util.ReadInput("day01/day01-input.txt")
+	
+	sum := 0
+
+	for _, line := range lines {
+		line = strings.ToLower(line)
+
+		firstIndex := -1
+		lastIndex := -1
+		firstDigit := ""
+		lastDigit := ""
+
+		numbers := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+		numberMap := make(map[string]string)
+
+		numberMap["one"] = "1"
+		numberMap["two"] = "2"
+		numberMap["three"] = "3"
+		numberMap["four"] = "4"
+		numberMap["five"] = "5"
+		numberMap["six"] = "6"
+		numberMap["seven"] = "7"
+		numberMap["eight"] = "8"
+		numberMap["nine"] = "9"
+		
+		for _, number := range numbers {
+			index1 := strings.Index(line, number)
+			index2 := strings.LastIndex(line, number)
+			
+			if index1 != -1 {
+				if firstIndex == -1 {
+					firstIndex = index1
+					firstDigit = number
+				}
+				if firstIndex >= index1 {
+					firstIndex = index1
+					firstDigit = number
+				}
+			}
+
+			if index2 != -1 {
+				if lastIndex == -1 {
+					lastIndex = index2
+					lastDigit = number
+				}
+				if lastIndex < index2 {
+					lastIndex = index2
+					lastDigit = number
+				}
+			}
+		}
+
+		if len(firstDigit) > 1 {
+			firstDigit = numberMap[firstDigit]
+		}
+
+		if len(lastDigit) > 1 {
+			lastDigit = numberMap[lastDigit]
+		}
+
+		fullNumber := firstDigit
+
+		if lastDigit != "0" {
+			fullNumber= firstDigit + lastDigit
+		}
+
+		number, err := strconv.Atoi(fullNumber)
+		
+		util.Check(err)
+		
+		sum += number
+	}
+
+	fmt.Println(sum)
 }
